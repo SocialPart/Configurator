c6398b7e03f9d6629f15982164360b652f8e3659
SocialPart <63605538+SocialPart@users.noreply.github.com>
2024-02-22 20:55:59 +0500

    Добавил бэкап с разными ТИ, ТС, ТУ. Дописал класс DiscretePoint

--- warehouse_classes.py
+++ warehouse_classes.py
@@ -17,9 +17,23 @@ class AnalogPoint:
 
 
 class DiscretePoint:
-    def __init__(self, name: str = '', signal_type: int = 10, naming: str = '',):
+    def __init__(self, name: str = '', signal_type: int = 1, naming: str = '',
+                 formula: str = None, formula_time: str = None, aging: int = None,
+                 invert: int = None):
+        self.name = name
+        self.c = 1
+        self.signal_type = signal_type
+        self.naming = naming
+        self.formula = formula
+        self.formula_time = formula_time
+        self.aging = aging
+        self.invert = invert
+
+class CommandPoint:
+    def __init__(self):
 
 
 
-s = analog_point()
+
+s = AnalogPoint()
 print(s.c)
