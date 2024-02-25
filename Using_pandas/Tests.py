5f23ca08475294b7f3607e9df583bb4fcf4f985b
SocialPart <63605538+SocialPart@users.noreply.github.com>
2024-02-25 17:25:55 +0500

    Дополнил файл с тестами. Оформил методы в Warehouse классе

--- Using_pandas/Tests.py
+++ Using_pandas/Tests.py
@@ -1,14 +1,28 @@
 import parsers
-#import classes
+import classes
 
-# Проверка корректности создания датафрейма Warehouse под Points
-wh = parsers.parse_warehouse()
-print(wh['points'])
-wh_filename = 'warehouse_pointsw.xlsx'
-wh['points'].to_excel(wh_filename)
+"""Проверка корректности создания датафрейма Warehouse под Points"""
 
-# Проверка корректности создания датафрейма Kernel под Points
-ker = parsers.parse_kernel()
-print(ker['points'])
-ker_filename = 'kernel_points.xlsx'
-ker['points'].to_excel(ker_filename)
+# wh = parsers.parse_warehouse()
+# print(wh['points'])
+# wh_filename = 'warehouse_pointsw.xlsx'
+# wh['points'].to_excel(wh_filename)
+# wh_command_filename = 'warehouse_commands.xlsx'
+# wh['commands'].to_excel(wh_command_filename)
+
+"""Проверка корректности создания датафрейма Kernel под Points"""
+
+# ker = parsers.parse_kernel()
+# print(ker['points'])
+# ker_filename = 'kernel_points.xlsx'
+# ker['points'].to_excel(ker_filename)
+
+"Проверка конструктора экземпляра класса для Warehouse и проверка методов"
+
+wh = classes.Warehouse()
+print(wh)
+print(wh.points)
+wh.add_discrete(name='Testing')
+wh_points_filename = 'wh_points.xlsx'
+wh.points.to_excel(wh_points_filename)
+
--- Using_pandas/classes.py
+++ Using_pandas/classes.py
@@ -1,6 +1,42 @@
-#classes.py
+# classes.py
+import parsers
 
 """Описать классы и методы для работы с конфигуратором"""
 
+
 class Warehouse:
     def __init__(self):
+        self.warehouse = parsers.parse_warehouse()
+        self.points = self.warehouse['points']
+        self.commands = self.warehouse['commands']
+        pass
+
+    def add_discrete(self, name: str, c: str = '1', signal_type: str = '1', naming: str = None,
+                     formula: str = None, formula_time: str = None, aging: str = None, invert: str = None):
+        discrete = {'name': name, 'c': c, 'signal_type': signal_type, 'naming': naming,
+                    'formula': formula, 'formula_time': formula_time, 'aging': aging, 'invert': invert}
+        self.points.loc[name] = discrete
+
+    def add_analog(self, name: str, c: str = '0', signal_type: str = '12', naming: str = None,
+                   lo: str = None, hi: str = None, var: str = None, abs_var: str = None,
+                   formula: str = None, formula_time: str = None, aging: str = None):
+        analog = {'name': name, 'c': c, 'signal_type': signal_type, 'naming': naming,
+                  'lo': lo, 'hi': hi, 'var': var, 'abs_var': abs_var,
+                  'formula': formula, 'formula_time': formula_time, 'aging': aging}
+        self.points.loc[name] = analog
+
+    def add_command(self, name: str, c: str = '1', signal_type: str = '1', naming: str = None,
+                    lo: str = None, hi: str = None, var: str = None, abs_var: str = None,
+                    formula: str = None, formula_time: str = None, aging: str = None):
+        analog = {'name': name, 'c': c, 'signal_type': signal_type, 'naming': naming,
+                  'lo': lo, 'hi': hi, 'var': var, 'abs_var': abs_var,
+                  'formula': formula, 'formula_time': formula_time, 'aging': aging}
+        self.points.loc[name] = analog
+
+
+class Kernel:
+    def __init__(self):
+        self.kernel = parsers.parse_kernel()
+        self.points = self.kernel['points']
+        self.commands = self.kernel['commands']
+        pass
